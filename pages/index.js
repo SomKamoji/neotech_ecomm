0d11fdb9decffa410316e6fb3c84f366401608fe
Som Kamoji <somkamoji01@gmail.com>
2022-07-20 23:10:24 +0530

    fifth commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ products , bannerData}) => (
     {console.log(bannerData)}
     <div className='products-heading'>
       <h2>Best Super Selling Products</h2>
-      <p>Speakers of many variations</p>
+      <p>Popular Speakers of many variations</p>
     </div>
     <div className='products-container'>
       {products?.map((product)=> <Product key={product._id} product={product}/>)}
